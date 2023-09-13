79c5c891f84f77f33dd52bfcf2ef6c9fd10be053
Rojoxt <delamarzp@gmail.com>
2023-09-12 23:17:00 -0500

    add nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SoftRoute-Frontend';
+  profile = 'supplier';
+  id = 1;
+  options = [
+    { path: `/supplier-home/${this.id}`, title: 'Home'},
+    { path: '', title: 'Plans'},
+    { path: '', title: 'Notifications'},
+    { path: `/supplier-inventory/${this.id}`, title: 'Inventory'},
+    { path: `/supplier-profile/${this.id}`, title: 'Profile'}
+  ]
+  /*Store*/
+  options2 = [
+    { path: `/store-home/${this.id}`, title: 'Home'},
+    { path: '', title: 'Orders'},
+    { path: '', title: 'Notifications'},
+    { path: `/store-profile/${this.id}`, title: 'Profile'}
+  ]
 }
