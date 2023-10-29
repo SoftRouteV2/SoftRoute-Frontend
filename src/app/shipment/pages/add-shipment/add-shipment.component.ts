807df5cead0ce81138218e970054c34a63665a9b
MICHAELCARRILLO <u20201a924@upc.edu.pe>
2023-10-29 17:16:34 -0500

    feat: added shipment component

--- src/app/shipment/pages/add-shipment/add-shipment.component.ts
+++ src/app/shipment/pages/add-shipment/add-shipment.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-shipment',
@@ -13,10 +15,38 @@ export class AddShipmentComponent {
   consigneeName: string='';
   consigneeEmail: string='';
   selectedDate: Date=new Date();
+  destinations = ["Cusco", "Puno", "Lima", "Cajamarca"]
+  selectedDestination: string='';
+  documentTypes = ["DNI", "Carnet de extranjeria", "Pasaporte"]
+  selectedDocumentType: string='';
+  packageTypes = ["Caja", "Sobre", "Bolsa"]
+  selectedPackageType: string='';
+  packageWeight: number | undefined;
+  quantity: number| undefined;
+  freight: number | undefined;
+  documentNumber: number | undefined;
+
+  numberInput: FormControl = new FormControl('', [Validators.required, Validators.pattern(/^\d+(\.\d+)?$/)]);
+  senderEmailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  consigneeEmailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  validateShimentData(): void {
+
+
+  }
+
+
 }
+
+
+
+
+
+
