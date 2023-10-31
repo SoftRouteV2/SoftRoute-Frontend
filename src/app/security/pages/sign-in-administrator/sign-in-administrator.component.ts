461973a27ae616bc3423c9b25045da7b346c466d
Abel Cierto <u20201b561@upc.edu.pe>
2023-10-31 15:52:22 -0500

    feat: login employee

--- src/app/security/pages/sign-in-administrator/sign-in-administrator.component.ts
+++ src/app/security/pages/sign-in-administrator/sign-in-administrator.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import {routes} from "../../../app-routing.module";
 import {AuthService} from "../../services/auth.service";
 import { Employee } from '../../model/employee';
+import { FormGroup, FormBuilder, FormControl, Validators  } from '@angular/forms';
 
 
 
@@ -13,17 +14,25 @@ import { Employee } from '../../model/employee';
 })
 export class SignInAdministratorComponent{
 
-  email: string='';
-  password: string='';
   employee: Employee | null = null;
-  constructor(private router: Router, private authService: AuthService) {
+
+  loginForm!: FormGroup;
+
+  constructor(private router: Router, private authService: AuthService, private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', {validators: [Validators.required, Validators.email], updateOn: 'change'}),
+      password: new FormControl('', { validators:  [Validators.required, Validators.minLength(4), Validators.maxLength(16)], updateOn: 'change' }),
+    });
   }
 
   navigateSignInHome() {
-    this.authService.loginEmployeeByEmail(this.email).subscribe((data: any) => {
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+    this.authService.loginEmployeeByEmail(email).subscribe((data: any) => {
       this.employee = data;
       if (this.employee != null) {
-        if (this.employee.password == this.password && this.employee.email == this.email) {
+        if (this.employee.password == password && this.employee.email == email) {
+          localStorage.setItem('employeeId', JSON.stringify(this.employee.employeeId));
           this.router.navigate([routes.supplierHome]);
         } else {
           alert("Contraseña incorrecta");
