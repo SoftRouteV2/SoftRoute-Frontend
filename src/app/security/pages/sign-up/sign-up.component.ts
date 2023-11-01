8fe07c74dc5dedf1a4e43b72074286603c866776
Abel Cierto <u20201b561@upc.edu.pe>
2023-11-01 10:27:04 -0500

    feat: finished registered a user

--- src/app/security/pages/sign-up/sign-up.component.ts
+++ src/app/security/pages/sign-up/sign-up.component.ts
@@ -29,6 +29,9 @@ export class SignUpComponent implements OnInit{
   companies: Array<Company> = [];
   selectedCompany: Company;
   registerForm!: FormGroup;
+  visible: boolean = false;
+  visible2: boolean = false;
+  registered: boolean = false;
 
   constructor(private router: Router, private employeeService:EmployeeService,
     private companyService:CompanyService, private messageService:MessageService, 
@@ -45,8 +48,6 @@ export class SignUpComponent implements OnInit{
       validators: this.MustMatch( 'password', 'confirmPassword')
     },
     );
-
-
     this.selectedCompany = {} as Company;
     this.newEmployee = {} as Employee;
 
@@ -57,6 +58,13 @@ export class SignUpComponent implements OnInit{
       this.companies = data; // Guarda todas las empresas en this.companies      
       this.selectedCompany = this.companies[0];
     });
+  }
+
+  showDialog() {
+    this.visible = true;
+  }
+  showDialog2() {
+    this.visible = true;
   }
 
   MustMatch( password: any, confirmPassword: any) {
@@ -99,12 +107,34 @@ export class SignUpComponent implements OnInit{
       this.newEmployee.employeeName = this.registerForm.value.employeeName;
       this.newEmployee.password = this.registerForm.value.password;
       this.newEmployee.email = this.registerForm.value.email;
+      this.verifyUserUnregistered();
+      if(!this.registered) {
+        this.authService.postEmployee(this.newEmployee, this.selectedCompany.companyId).subscribe((data: Employee) => {
+          this.newEmployee = data;
+          this.showNewEmployeeSuccess();
+          this.showDialog2();
+          setTimeout(() => {
+            this.navigateToLogin();
+          }, 3000);
+        });
+      }
+      else {
+        this.showDialog();
+      }
+    }
+    
+  }
 
-      this.authService.postEmployee(this.newEmployee, this.selectedCompany.companyId).subscribe((data: Employee) => {
-        this.newEmployee = data;
-        this.showNewEmployeeSuccess();
-        this.navigateToLogin();
-      });
+  verifyUserUnregistered() {
+    this.registered = false;
+    var req = new XMLHttpRequest();
+    req.open('GET', `http://localhost:8081/api/v1/employee/email/${this.registerForm.get("email")?.value}`, false);
+    req.send(null);
+    if (req.status == 200) {
+      var user = JSON.parse(req.responseText);
+      if (user.email == this.registerForm.get('email')?.value) {
+        this.registered = true;
+      }
     }
     
   }
